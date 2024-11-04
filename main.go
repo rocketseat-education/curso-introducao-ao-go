e695dd2fc4dafc860a762ade66ff8a014f5477da
Luis Fellipe Batista Silva <luis.batista@irricontrol.com.br>
2024-11-04 20:23:54 -0300

    Tipo int

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import "fmt"
 
-const nome = "lais"
+// int8: O intervalo de valores é de -128 a 127. 
+// int16: O intervalo de valores é de -32.768 a 32.767. 
+// int32: O intervalo de valores é de -2.147.483.648 a 2.147.483.647. 
+// int64: O intervalo de valores é de -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807.
 
 func main() {
-	fmt.Println(nome)
+	var idade int = 30
+	var contador int32 = 2
+	var indice int8 = 1
+
+	fmt.Println("Idade:", idade )
 }
