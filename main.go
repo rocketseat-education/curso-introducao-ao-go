4db48b0820e121acf81f843deae1d3922abcda55
Luis Fellipe Batista Silva <luis.batista@irricontrol.com.br>
2024-11-04 20:27:30 -0300

    Tipos float

--- main.go
+++ main.go
@@ -2,15 +2,13 @@ package main
 
 import "fmt"
 
-// int8: O intervalo de valores é de -128 a 127. 
-// int16: O intervalo de valores é de -32.768 a 32.767. 
-// int32: O intervalo de valores é de -2.147.483.648 a 2.147.483.647. 
-// int64: O intervalo de valores é de -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807.
-
 func main() {
-	var idade int = 30
-	var contador int32 = 2
-	var indice int8 = 1
+	
+	var floatNumber float32 = 1.1
+	var pi float64 = 3.14
+	var raio float64 = 2.5
+	var area = pi * raio * raio
 
-	fmt.Println("Idade:", idade )
+	fmt.Println("floatNumber:", floatNumber )
+	fmt.Println("Área do círculo:", area )
 }
