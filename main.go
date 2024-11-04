0b631db017b0f203f120a0c7708b967decd862e0
Luis Fellipe Batista Silva <luis.batista@irricontrol.com.br>
2024-11-04 20:30:10 -0300

    Tipo bool

--- main.go
+++ main.go
@@ -4,11 +4,8 @@ import "fmt"
 
 func main() {
 	
-	var floatNumber float32 = 1.1
-	var pi float64 = 3.14
-	var raio float64 = 2.5
-	var area = pi * raio * raio
-
-	fmt.Println("floatNumber:", floatNumber )
-	fmt.Println("Área do círculo:", area )
+	var maior bool = 10 > 5
+	var menor bool = 10 < 5
+	fmt.Println("10 é maior que 5?", maior)
+	fmt.Println("10 é menor que 5?", menor)
 }
