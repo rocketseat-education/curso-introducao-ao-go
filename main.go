d341d53b0fe5581759781ef4f3b6f3770a089125
Luis Fellipe Batista Silva <luis.batista@irricontrol.com.br>
2024-11-05 08:27:37 -0300

    Map

--- main.go
+++ main.go
@@ -5,8 +5,15 @@ import (
 )
 
 func main() {
-	var gavetas []string
-	gavetas = append(gavetas, "copos", "panos", "pratos")
-	gavetas = gavetas[:2]
-	fmt.Println(gavetas)
+	var pessoas = map[string]string{}
+	pessoas["lais"] = 26
+	pessoas["leo"] = 32
+
+	if idade, ok := pessoas["lais"]; ok {
+		fmt.Println("Pesssoa existe no map", idade, ok)
+	} else {
+		fmt.Println("Pessoa não existe no map")
+	}
+	delete(pessoas, "lais")
+	fmt.Println(pessoas)
 }
