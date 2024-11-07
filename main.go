d5188a71782d195f46ed4914cfa7e7c6bd60eeb4
Luis Fellipe Batista Silva <luis.batista@irricontrol.com.br>
2024-11-06 21:09:31 -0300

    Switch Case

--- main.go
+++ main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	
-	players := map[string]int{
-		"lais": 26,
-	}
+	fmt.Println("Quando é sábado?")
+	today := time.Now().Weekday()
 
-	if value, ok := players["eduardo"]; ok {
-		fmt.Println("pontos:", value, ok)
+	switch time.Saturday {
+		case today + 0:
+			fmt.Println("é hoje")
+		case today + 1:
+			fmt.Println("é amanhã")
+		case today + 1:
+			fmt.Println("é em dois dias")
+		default:
+			fmt.Println("Tá longe ainda ...")
 	}
 }
